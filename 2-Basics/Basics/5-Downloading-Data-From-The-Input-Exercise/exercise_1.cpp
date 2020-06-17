4acd47a11e11a4264577e076b06287dbaa7ef19b
SimonTanner <simonthetanner@hotmail.com>
2020-06-17 13:37:32 +0100

    input exercises

---
+++ 2-Basics/Basics/5-Downloading-Data-From-The-Input-Exercise/exercise_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main () {
+    string a, b, c;
+
+    cout << "Please enter your favourite animal: ";
+    cin >> a;
+    cout << "Please enter your favourite colour: ";
+    cin >> b;
+    cout << "Please enter your name: ";
+    cin >> c;
+
+    cout << c << ", " << "you are a " << b << " " << a << "!" << endl;
+}
---
+++ 2-Basics/Basics/5-Downloading-Data-From-The-Input-Exercise/exercise_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main () {
+    int a, b;
+
+    cout << "Please enter number a = ";
+    cin >> a;
+    cout << "Please enter number b = ";
+    cin >> b;
+
+    cout << "a = " << b << endl;
+    cout << "b = " << a << endl;
+}
